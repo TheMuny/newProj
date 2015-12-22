a5bf98cffa905f957fbbf82c0bb347d22f6e39a0
Munir <gm.BlackGod@gmail.com>
2015-12-22 14:08:43 +0200

    Update Main.java

--- DBJavaRequests/newProj/Main.java
+++ DBJavaRequests/newProj/Main.java
@@ -15,7 +15,7 @@ public class Main {
     
     
 	public static void main(String[] args) {
-		Connection conn = null;
+	Connection conn = null;
         Statement stmt = null;
         try{
         Class.forName(JDBC_DRIVER);
